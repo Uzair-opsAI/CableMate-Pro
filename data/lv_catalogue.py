d89375e33543dd853172a217a216316a15f7eeb8
Uzair-opsAI <uzair.saiyed786@gmail.com>
2026-04-24 15:20:30 +0530

    lv_catalogue.py

---
+++ data/lv_catalogue.py
@@ -0,0 +1,100 @@
+# LV Cable Catalogue Data (Oman Cables based)
+
+lv_cables_cu = [
+    {
+        "size": 1.5,
+        "current_air": 19,
+        "current_ground": 16,
+        "mv_per_am": 33,
+        "resistance": 12.1,
+        "reactance": 0.08
+    },
+    {
+        "size": 2.5,
+        "current_air": 27,
+        "current_ground": 22,
+        "mv_per_am": 22,
+        "resistance": 7.41,
+        "reactance": 0.08
+    },
+    {
+        "size": 4,
+        "current_air": 34,
+        "current_ground": 28,
+        "mv_per_am": 28,
+        "resistance": 4.61,
+        "reactance": 0.08
+    },
+    {
+        "size": 6,
+        "current_air": 44,
+        "current_ground": 36,
+        "mv_per_am": 36,
+        "resistance": 3.08,
+        "reactance": 0.08
+    },
+    {
+        "size": 10,
+        "current_air": 58,
+        "current_ground": 48,
+        "mv_per_am": 48,
+        "resistance": 1.83,
+        "reactance": 0.08
+    },
+    {
+        "size": 16,
+        "current_air": 83,
+        "current_ground": 70,
+        "mv_per_am": 70,
+        "resistance": 1.15,
+        "reactance": 0.08
+    },
+    {
+        "size": 25,
+        "current_air": 105,
+        "current_ground": 88,
+        "mv_per_am": 88,
+        "resistance": 0.727,
+        "reactance": 0.08
+    },
+    {
+        "size": 35,
+        "current_air": 129,
+        "current_ground": 109,
+        "mv_per_am": 109,
+        "resistance": 0.524,
+        "reactance": 0.08
+    },
+    {
+        "size": 50,
+        "current_air": 157,
+        "current_ground": 132,
+        "mv_per_am": 132,
+        "resistance": 0.387,
+        "reactance": 0.08
+    },
+    {
+        "size": 70,
+        "current_air": 200,
+        "current_ground": 167,
+        "mv_per_am": 167,
+        "resistance": 0.268,
+        "reactance": 0.08
+    },
+    {
+        "size": 95,
+        "current_air": 246,
+        "current_ground": 207,
+        "mv_per_am": 207,
+        "resistance": 0.193,
+        "reactance": 0.08
+    },
+    {
+        "size": 120,
+        "current_air": 288,
+        "current_ground": 241,
+        "mv_per_am": 241,
+        "resistance": 0.153,
+        "reactance": 0.08
+    },
+]
